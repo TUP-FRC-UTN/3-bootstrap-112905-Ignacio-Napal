6307ebd966da3e1316e7160b4d201dda09e68474
2w2-112905-Napal-Ignacio <112905@tecnicatura.utn.frc.edu.ar>
2022-04-03 18:43:13 -0300

    Se terminan las secciones restantes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const validateEmail = (email) => {
-  return email.match(
-    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
   );
 };
 
@@ -11,7 +11,11 @@ const sendMail = (mail, subject, body) => {
 //Elements
 const workP = document.querySelector("#work-p");
 const workH1 = document.querySelector("#work-h1");
-const nameInput = document.querySelector("#name-input");
+
+const emailField = document.querySelector("#email");
+const nameField = document.querySelector("#name-input");
+const submitButton = document.querySelector("#btn-submit");
+const form = document.getElementById("contact-form");
 
 const focusOnContactInput = (...elements) => {
   elements.forEach((element) => {
@@ -20,5 +24,22 @@ const focusOnContactInput = (...elements) => {
     });
   });
 };
-
 focusOnContactInput(workP, workH1);
+
+submitButton.addEventListener("click", () => {
+  if (nameField.value !== "") {
+    nameField.classList.remove("is-invalid");
+    nameField.classList.add("is-valid");
+  } else {
+    nameField.classList.remove("is-valid");
+    nameField.classList.add("is-invalid");
+  }
+
+  if (validateEmail(emailField.value)) {
+    emailField.classList.remove("is-invalid");
+    emailField.classList.add("is-valid");
+  } else {
+    emailField.classList.remove("is-valid");
+    emailField.classList.add("is-invalid");
+  }
+});
